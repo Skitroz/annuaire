3130d68e22d312d3748659e22239add8df63a33a
Skitroz <matheo08batelier@gmail.com>
2023-10-08 11:20:38 +0200

    Premier commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Annuaire - Normandie Web School</title>
+</head>
+
+<body>
+    <?php require_once('templates/layouts/header.layout.php'); ?>
+    <h1>Bonjour</h1>
+</body>
+
+</html>
---
+++ templates/layouts/header.layout.php
@@ -0,0 +1,7 @@
+<header class="absolute top-0 w-screen">
+    <div class="h-10 bg-[#ca4b38]"></div>
+    <nav class="shadow-xl py-5 px-24">
+        <img width="140" src="https://normandiewebschool.fr/uploads/logo_nws.svg"
+            alt="L'école des métiers du numérique">
+    </nav>
+</header>
