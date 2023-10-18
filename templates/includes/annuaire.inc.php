512e3061e4d12458ce1395fb64e9728aa1eaba34
Skitroz <matheo08batelier@gmail.com>
2023-10-18 18:32:59 +0200

    Finition

--- templates/includes/annuaire.inc.php
+++ templates/includes/annuaire.inc.php
@@ -14,32 +14,39 @@
 
     <div class="mb-10 mt-[200px] flex flex-col">
         <div class="flex justify-center items-center gap-16 mb-4">
-            <button
-                class="shadow-[0_1px_10px_1px_rgba(0,0,0,0.4)] p-4 rounded text-[#00a5a5] font-semibold transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
-                onclick="filtrerParSpecialite('Tous')">Tous</button>
-            <button
-                class="shadow-[0_1px_10px_1px_rgba(0,0,0,0.4)] p-4 rounded text-[#00a5a5] font-semibold transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
-                onclick="filtrerParSpecialite('Développement Web')">Développement Web</button>
-            <button
-                class="shadow-[0_1px_10px_1px_rgba(0,0,0,0.4)] p-4 rounded text-[#00a5a5] font-semibold transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
-                onclick="filtrerParSpecialite('Web Marketing')">Web Marketing</button>
-            <button
-                class="shadow-[0_1px_10px_1px_rgba(0,0,0,0.4)] p-4 rounded text-[#00a5a5] font-semibold transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
-                onclick="filtrerParSpecialite('Communication Graphique')">Communication Graphique</button>
-            <button
-                class="shadow-[0_1px_10px_1px_rgba(0,0,0,0.4)] p-4 rounded text-[#00a5a5] font-semibold transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
-                onclick="filtrerParSpecialite('Community Management')">Community Management</button>
+            <div id="specialite-bouton">
+                <button
+                    class="shadow-[0_1px_10px_1px_rgba(0,0,0,0.4)] p-4 rounded text-[#00a5a5] font-semibold transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 bouton-specialite"
+                    onclick="filtrerParSpecialite('Tous')">Tous</button>
+                <button
+                    class="shadow-[0_1px_10px_1px_rgba(0,0,0,0.4)] p-4 rounded text-[#00a5a5] font-semibold transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 bouton-specialite"
+                    onclick="filtrerParSpecialite('Développement Web')">Développement Web</button>
+                <button
+                    class="shadow-[0_1px_10px_1px_rgba(0,0,0,0.4)] p-4 rounded text-[#00a5a5] font-semibold transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 bouton-specialite"
+                    onclick="filtrerParSpecialite('Web Marketing')">Web Marketing</button>
+                <button
+                    class="shadow-[0_1px_10px_1px_rgba(0,0,0,0.4)] p-4 rounded text-[#00a5a5] font-semibold transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 bouton-specialite"
+                    onclick="filtrerParSpecialite('Communication Graphique')">Communication Graphique</button>
+                <button
+                    class="shadow-[0_1px_10px_1px_rgba(0,0,0,0.4)] p-4 rounded text-[#00a5a5] font-semibold transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 bouton-specialite"
+                    onclick="filtrerParSpecialite('Community Management')">Community Management</button>
+            </div>
         </div>
         <div class="flex justify-center items-center gap-16">
-            <button
-                class="shadow-[0_1px_10px_1px_rgba(0,0,0,0.4)] p-4 rounded text-[#00a5a5] font-semibold transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
-                onclick="filtrerParAnnee('A1')">A1</button>
-            <button
-                class="shadow-[0_1px_10px_1px_rgba(0,0,0,0.4)] p-4 rounded text-[#00a5a5] font-semibold transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
-                onclick="filtrerParAnnee('A2')">A2</button>
-            <button
-                class="shadow-[0_1px_10px_1px_rgba(0,0,0,0.4)] p-4 rounded text-[#00a5a5] font-semibold transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
-                onclick="filtrerParAnnee('A3')">A3</button>
+            <div id="annee-bouton">
+                <button
+                    class="shadow-[0_1px_10px_1px_rgba(0,0,0,0.4)] p-4 rounded text-[#00a5a5] font-semibold transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 bouton-annee"
+                    onclick="filtrerParAnnee('Tous')">Tous</button>
+                <button
+                    class="shadow-[0_1px_10px_1px_rgba(0,0,0,0.4)] p-4 rounded text-[#00a5a5] font-semibold transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 bouton-annee"
+                    onclick="filtrerParAnnee('A1')">A1</button>
+                <button
+                    class="shadow-[0_1px_10px_1px_rgba(0,0,0,0.4)] p-4 rounded text-[#00a5a5] font-semibold transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 bouton-annee"
+                    onclick="filtrerParAnnee('A2')">A2</button>
+                <button
+                    class="shadow-[0_1px_10px_1px_rgba(0,0,0,0.4)] p-4 rounded text-[#00a5a5] font-semibold transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 bouton-annee"
+                    onclick="filtrerParAnnee('A3')">A3</button>
+            </div>
         </div>
         <div class="mx-auto mt-[86px]">
             <button
@@ -112,25 +119,60 @@
         </div>
 
         <script>
+            let specialiteFiltre = 'Tous';
+            let anneeFiltre = 'Tous';
+
             function filtrerParSpecialite(specialite) {
+                specialiteFiltre = specialite; // Mettez à jour la valeur du filtre de spécialité
                 const tableau = document.querySelectorAll('#tableau tbody tr');
+                const boutonSpecialite = document.querySelectorAll('#specialite-bouton .bouton-specialite');
+
+                boutonSpecialite.forEach(b => {
+                    b.classList.remove('bg-[#00a5a5]', 'text-white');
+                });
+
+                boutonSpecialite.forEach(bouton => {
+                    if (bouton.textContent === specialite) {
+                        bouton.classList.add('bg-[#00a5a5]', 'text-white');
+                    }
+                });
+
                 tableau.forEach(function (tableau) {
                     const colonneSpecialite = tableau.querySelector('td:nth-child(5)').textContent;
-                    if (specialite === 'Tous' || colonneSpecialite === specialite) {
+                    const colonneAnnee = tableau.querySelector('td:nth-child(4)').textContent;
+
+                    if ((specialiteFiltre === 'Tous' || colonneSpecialite === specialiteFiltre) &&
+                        (anneeFiltre === 'Tous' || colonneAnnee === anneeFiltre)) {
                         tableau.style.display = 'table-row';
                     } else {
                         tableau.style.display = 'none';
                     }
                 });
             }
-            // Appelez cette fonction pour afficher tous les étudiants au chargement de la page.
+
             filtrerParSpecialite('Tous');
 
             function filtrerParAnnee(annee) {
+                anneeFiltre = annee; // Mettez à jour la valeur du filtre d'année
                 const tableau = document.querySelectorAll('#tableau tbody tr');
+                const boutonAnnee = document.querySelectorAll('#annee-bouton .bouton-annee');
+
+                boutonAnnee.forEach(b => {
+                    b.classList.remove('bg-[#00a5a5]', 'text-white');
+                });
+
+                boutonAnnee.forEach(bouton => {
+                    if (bouton.textContent === annee) {
+                        bouton.classList.add('bg-[#00a5a5]', 'text-white');
+                    }
+                });
+
                 tableau.forEach(function (tableau) {
+                    const colonneSpecialite = tableau.querySelector('td:nth-child(5)').textContent;
                     const colonneAnnee = tableau.querySelector('td:nth-child(4)').textContent;
-                    if (annee === 'Tous' || colonneAnnee === annee) {
+
+                    if ((specialiteFiltre === 'Tous' || colonneSpecialite === specialiteFiltre) &&
+                        (anneeFiltre === 'Tous' || colonneAnnee === anneeFiltre)) {
                         tableau.style.display = 'table-row';
                     } else {
                         tableau.style.display = 'none';
--- templates/layouts/header.layout.php
+++ templates/layouts/header.layout.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <header class="absolute bg-white top-0 w-screen z-50">
     <div class="h-8 bg-[#ca4b38]">
         <p class="text-center my-auto text-white font-semibold pt-1">Nouvel annuaire de la Normandie Web School !</p>
@@ -6,12 +10,21 @@
         <a href="/annuaire"><img width="140" src="https://normandiewebschool.fr/uploads/logo_nws.svg"
                 alt="L'école des métiers du numérique"></a>
         <ul class="flex gap-8">
-            <li class="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"><a href="/annuaire/templates/includes/annuaire.inc.php"
+            <li class="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+                <a href="/annuaire/templates/includes/annuaire.inc.php"
                     class="shadow-[0_1px_10px_1px_rgba(0,0,0,0.4)] p-4 rounded text-[#00a5a5] font-semibold">Annuaire</a>
             </li>
-            <li class="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"><a href="/annuaire/templates/includes/connexion.inc.php" class="shadow-[0_1px_10px_1px_rgba(0,0,0,0.4)] p-4 rounded text-[#00a5a5] font-semibold">Mon
-                    compte</a></li>
+            <li class="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+                <?php
+                if (isset($_SESSION['username'])) {
+                    echo '<a href="/annuaire/templates/includes/deconnexion.inc.php" class="shadow-[0_1px_10px_1px_rgba(0,0,0,0.4)] p-4 rounded text-[#00a5a5] font-semibold">Déconnexion</a>';
+                } else {
+                    echo '<a href="/annuaire/templates/includes/connexion.inc.php" class="shadow-[0_1px_10px_1px_rgba(0,0,0,0.4)] p-4 rounded text-[#00a5a5] font-semibold">Mon compte</a>';
+                }
+                ?>
+            </li>
         </ul>
-        
+
+
     </nav>
 </header>
